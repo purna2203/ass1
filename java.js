ba830d45b59b21a114e0494b416cfed3d0916f51
purna 2203 <purnashri.r.r3895@gmail.com>
2025-06-09 19:45:21 +0530

    first commit

---
+++ java.js
@@ -0,0 +1,16 @@
+console.log('hello from script');
+ console.log(5==='5');
+ console.log(5=='5');
+ let i=0
+ do{
+    i++;
+    console.log(i);
+ }while(i<5)
+   function greet(name ,age){
+      return ` Hello ${name }  is ${ age } old`
+      }
+   console.log(greet('Varshine',18));
+   function add(a,b){
+      return (a+b)
+   }
+console.log(add(10,20))
